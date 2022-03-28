f246035a1003096730788574094c90e543b7fe2c
DorelGutanov <dgutanov9@mail.ru>
2022-03-29 00:27:47 +0300

    I am resolve all errors

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const StoreContext = React.createContext(null)
+const StoreContext = React.createContext<any | null>(null)
 export const useRootStore =()=>React.useContext(StoreContext)
-const StoreProvider =({children }):{children:ReactElement}=>{
+const StoreProvider =({children }:{children:ReactElement})=>{
     return(
         <StoreContext.Provider value={StoreProvider}>
             {children}
